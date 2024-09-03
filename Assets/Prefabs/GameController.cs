1be08e5524536706766d88e80f7ef4d27c44543a
ThanutPro <Thanutpro@gmail.com>
2024-09-04 02:46:59 +0700

    Added Listeners + BG image

--- Assets/Prefabs/GameController.cs
+++ Assets/Prefabs/GameController.cs
@@ -11,20 +11,34 @@ public class GameController : MonoBehaviour
 
     public List<Button> btns = new List<Button>();
 
-     void Start()
+    void Start()
     {
         GetButtons();
-    }   
-    
+        AddListeners();
+    }
+
     void GetButtons()
     {
         GameObject[] objects = GameObject.FindGameObjectsWithTag("Puzzle Button");
 
-        for(int i = 0; i < objects.Length; i++)
+        for (int i = 0; i < objects.Length; i++)
         {
             btns.Add(objects[i].GetComponent<Button>());
             btns[i].image.sprite = bgImage;
         }
     }
 
+    void AddListeners()
+    {
+        foreach(Button btn in btns)
+        {
+            btn.onClick.AddListener(() => PickAPuzzle());
+        }
+    }
+
+    public void PickAPuzzle()
+    {
+        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        Debug.Log("Clicked " + name + " button");
+    }
 }
